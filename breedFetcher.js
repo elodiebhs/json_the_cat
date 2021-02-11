c4ba28c694efa57168a241e915eee207e769827d
elodie <elodiebouthors@hotmail.com>
2021-02-11 23:59:53 +0000

    updates with Mocha Test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,32 +1,30 @@
 const request = require('request');
 
-const breedName = process.argv[2];
-console.log(breedName);
 
-const breedFetch = function (breedName){
+const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function (error, response, body) {
     //console.log(typeof body) sting
 
     if (error) {
-      console.log("failed to dowload", error);
-      return ;
+      //console.log("failed to dowload", error);
+      callback(error, null);
+      return;
     } else {
       const data = JSON.parse(body);
       //console.log(typeof data) object ;
       if (!data[0]) { // OR (data.length === 0)
-        console.log(`Sorry, the breed '${breedName}' was not found.`);
-      return;
+        //console.log(`Sorry, the breed '${breedName}' was not found.`);
+        return callback((`Sorry, the breed '${breedName}' was not found.`), null)
+        
       }
-        console.log(data[0].description); // accessing the description in an array of object
-
+      //console.log(data[0].description); // accessing the description in an array of object
+      callback(null,(data[0].description))
     }
-
   });
 };
-breedFetch(breedName);
 
 
-module.exports = { breedFetch };
+module.exports = { fetchBreedDescription };
 
 
 
