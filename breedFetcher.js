b124715cd7a42c83b511c5a6ae2e6245f9859ad2
elodie <elodiebouthors@hotmail.com>
2021-02-11 22:10:06 +0000

    updates

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,16 +5,16 @@ console.log(breadName);
 
 const breedFetch = function(breadName){
   request (`https://api.thecatapi.com/v1/breeds/search?q=${breadName}`, function (error, response, body) {
-    //console.log(typeof body)
+    //console.log(typeof body) sting
     const data = JSON.parse(body);
-
+    //console.log(typeof data) object ;
     if (data.length === 0) {
       console.log(`Sorry, the breed '${breadName}' was not found.`);
       return;
     }
 
-    console.log(data[0].description);
-    //console.log(typeof data);
+    console.log(data[0].description); // accessing the description in an array of object
+    
 
   });
 };
