929ce2e0bf7d1eedfb4988fe4c7c65f03b84dd3c
elodie <elodiebouthors@hotmail.com>
2021-02-11 22:38:20 +0000

    updates with error

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,32 @@
 const request = require('request');
 
-const breadName = process.argv[2];
-console.log(breadName);
+const breedName = process.argv[2];
+console.log(breedName);
 
-const breedFetch = function(breadName){
-  request (`https://api.thecatapi.com/v1/breeds/search?q=${breadName}`, function (error, response, body) {
+const breedFetch = function (breedName){
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function (error, response, body) {
     //console.log(typeof body) sting
-    const data = JSON.parse(body);
-    //console.log(typeof data) object ;
-    if (data.length === 0) {
-      console.log(`Sorry, the breed '${breadName}' was not found.`);
-      return;
-    }
 
-    console.log(data[0].description); // accessing the description in an array of object
-    
+    if (error) {
+      console.log("failed to dowload", error);
+      return ;
+    } else {
+      const data = JSON.parse(body);
+      //console.log(typeof data) object ;
+      if (!data[0]) { // OR (data.length === 0)
+        console.log(`Sorry, the breed '${breedName}' was not found.`);
+      return;
+      }
+        console.log(data[0].description); // accessing the description in an array of object
+
+    }
 
   });
 };
-breedFetch(breadName);
+breedFetch(breedName);
+
+
+module.exports = { breedFetch };
 
 
 
---
+++ index.js
@@ -0,0 +1,1 @@
+const { breedFetch } = require('./breedFetcher');
